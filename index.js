f64f879e31ee5b9dc279f6041ab5de81346b0fef
Sơn ĐInh Trung <SON.DT13376@sinhvien.hoasen.edu.vn>
2023-02-07 10:26:43 +0700

    update file 10 fast fingers

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
+// ham random
+// dom de xac dinh thanh phan can de bat event 
+// bat event
+// kiem tra 
+// them, xoa, sua bang object.innerText
+// setinterval 
+// clearInterval
+
+
+// randome tu
 let arr=["hello","hi","world","shy","food","for","it","on","one","two","there","four","five","six","seven","eight","nine","ten"];
+// de random tu thi random index
+// (0 - 1) => (0 length of array)
+let indexRandom = Math.floor( Math.random() * arr.length);
+let test = document.getElementById("randomWord");
+// event 
+let a = document.getElementById("input-text");
+a.addEventListener("keypress", function (event) {
+    if(event.key === "Enter") {
+        test.innerText = "hello world";
+    }
+});
+
+
+
+
+
+
+
 let word=document.getElementById("content-random")
 const button=document.getElementById("btn-play")
 let input=document.getElementById("input")
 let score=document.getElementById("result")
 let countdowns=document.getElementById("countdown")
-
-function random(){
-    let itemrandom=Math.floor(Math.random()*arr.length);
-    return intemrandom
-}
-
-let number=0;
-let time=0;
-
-function checkWord(){
-    if(input.value==word.innerHTML){
-        word.innerHTML=arr[random()]
-        number++;
-        score.innerHTML=number;
-        input.value=""
-    }
-    else{
-        word.innerHTML=arr[random()]
-        input.value=""
-    }
-}
-    input.addEventListener("keyup",({key}) => {
-    if(key==="Enter"){
-        checkWord()
-    }
-})
